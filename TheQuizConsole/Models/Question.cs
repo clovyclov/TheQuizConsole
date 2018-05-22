528df02235375abe31aa6a27f4f211def10f9637
Clovis Shropshire <shropshireclovis@gmail.com>
2018-05-22 00:03:07 -0400

    Initial Commit

---
+++ TheQuizConsole/Models/Question.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheQuizConsole.Models
+{
+    interface IQuestion
+    {
+        int Id { get; set; }
+        string Description { get; set; }
+        Answer Answer { get; set; }
+    }
+}
